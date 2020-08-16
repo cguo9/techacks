9df417f2d986badf4f5d8061b8cffcc89875eb6e
Mariam Khan <mariamkhanofficial@gmail.com>
2020-08-16 01:04:59 -0400

    all image buttons working

--- app/src/main/java/com/example/testing/HomePage.java
+++ app/src/main/java/com/example/testing/HomePage.java
@@ -1,6 +1,9 @@
 package com.example.testing;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,25 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        ImageButton imgbtn_s3;
+        imgbtn_s3 = (ImageButton) findViewById(R.id.imgbtn_study3);
+        imgbtn_s3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imgbtn_c3;
+        imgbtn_c3 = (ImageButton) findViewById(R.id.imgbtn_collections3);
+        imgbtn_c3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, Collections.class);
+                startActivity(intent);
+            }
+        });
     }
 }
